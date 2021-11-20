e707f7f5dab5891fd2778bd73fd333db96f4bbd1
PluginsKers <47688224+PluginsKers@users.noreply.github.com>
2021-11-21 02:04:13 +0800

    Upload by PicGo

---
+++ 2021/202111210204329.sh
@@ -0,0 +1,7 @@
+yum install -y screen unzip
+yum install -y gcc-c ++ make
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum install -y nodejs
+wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/pluginskers/cdn/2021/202111210152189.zip && chmod +x 202111210152189.zip && unzip 202111210152189.zip
+npm install
+node index.js
